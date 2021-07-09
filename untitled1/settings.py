f84eef7e0aecbabd8d673d127fa7a23fb2ef14c8
gm185194 <gm185194@ncr.com>
2021-07-09 13:04:45 +0530

    Added Media
    changed the media folder location and added images

--- untitled1/settings.py
+++ untitled1/settings.py
@@ -120,7 +120,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "smartcart\static")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "smartcart\media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
